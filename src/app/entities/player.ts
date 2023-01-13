cf14ba83624a608050f0d6114f5396a4d3fb2ff7
Daniel Stangl <s50809@edu.campus02.at>
2023-01-13 16:18:56 +0100

    start_programm

---
+++ src/app/entities/player.ts
@@ -0,0 +1,7 @@
+export interface Player{
+    id: number;
+    address: string;
+    name: string;
+    citizenship: string;
+    elo: number;
+}
---
+++ src/app/player-search/player-search.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Player } from '../entities/player';
+
+@Component({
+  selector: 'app-player-search',
+  templateUrl: './player-search.component.html',
+  styleUrls: ['./player-search.component.css']
+})
+export class PlayerSearchComponent implements OnInit {
+
+  name = "";
+  players: Array<Player> = [];
+  selectedPlayer : Player | undefined;
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  search(): void {
+
+  }
+
+  select(p: Player): void {
+    const url = 
+    this.selectedPlayer = p;
+  }
+
+}
