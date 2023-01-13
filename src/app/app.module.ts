0ffce54ac267c2cb3bb6a753f2fe3a325d43193d
Barbara Kogler <00330653@edu.campus02.at>
2023-01-13 17:27:09 +0100

    httpClient gefixt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,24 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { FlightSearchComponent } from './flight-search/flight-search.component';
+//import { FlightSearchComponent } from './flight-search/flight-search.component';
 import { PlayerSearchComponent } from './player-search/player-search.component';
 import { MatchSearchComponent } from './match-search/match-search.component';
 import { LocationSearchComponent } from './location-search/location-search.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   declarations: [
     AppComponent,
     SidebarComponent,
     NavbarComponent,
-    FlightSearchComponent,
+    //FlightSearchComponent,
     PlayerSearchComponent,
     MatchSearchComponent,
     LocationSearchComponent
